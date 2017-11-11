cd28c573c9bcc4a11ae9379c1dfa4fd8c2d0a27b
Anand Raman <anand.raman@appdirect.com>
2017-11-10 19:07:14 -0800

    added node and npm version

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const { get, post, put, del } = require("server/router");
 const { json } = require("server/reply");
 
 const analyticScriptResponseJSON = require("./analyticScript");
+const LOGINJSON = require("./LOGIN");
 
 const analyticScriptResponse = ctx => json(analyticScriptResponseJSON);
+const LOGIN = ctx => json(LOGINJSON);
 
 server({ port: 3001 }, middleware, [
-  get("/api/analyticScript", analyticScriptResponse)
+  get("/api/analyticScript", analyticScriptResponse),
+  get("/api/analyticScript/LOGIN", LOGIN)
 ]);
